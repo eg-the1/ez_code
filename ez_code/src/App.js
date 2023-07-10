18d32fe676da162c00ada96c87d2e6d793b233fe
eg-the1 <kdskds96242337@gmail.com>
2023-07-11 00:16:40 +0900

    react porject 만듬

---
+++ ez_code/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="top-bar">
+        <p id='bar-text'>BETA_TEST</p>
+      </div>
+      <div className="line"></div>*50
+    </div>
+  );
+}
+
+export default App;
